fe651b421162b150e54a400b1667d0a5be980824
jweskamp <Jake.Weskamp@gmail.com>
2018-01-21 15:04:01 -0600

    Turned on RFID

--- puzzle_box/puzzle_box/puzzle_box.ino
+++ puzzle_box/puzzle_box/puzzle_box.ino
@@ -3,34 +3,69 @@
  Created:	1/21/2018 1:46:59 PM
  Author:	mc
 */
+
 /*------------------------------------------
-includes 
+Includes 
 ------------------------------------------*/
 #include <SoftwareSerial.h>
 #include <SerialGraphicLCD.h>
 
 /*------------------------------------------
+Constants
+------------------------------------------*/
+#define card "00006A263D71"
+
+/*------------------------------------------
 Object Declarations
 ------------------------------------------*/
-//SoftwareSerial LCD_serial = SoftwareSerial(2, 3);
-
+SoftwareSerial RFID = SoftwareSerial(5, 6);
 LCD myLCD;
+
+/*------------------------------------------
+Global Variables
+------------------------------------------*/
+char c;
+String msg;
 
 // the setup function runs once when you press reset or power the board
 void setup() 
 {
 	delay(1200);
 	pinMode(13, OUTPUT);
+	digitalWrite(13, LOW);
+
 	myLCD.setHome();
 	myLCD.printStr("here");
+
+
+	Serial.begin(9600);
+	RFID.begin(9600);
 
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() 
 {
-	digitalWrite(13, HIGH);
-	delay(500);
-	digitalWrite(13, LOW);
-	delay(500); 
+	while (RFID.available() > 0 && msg.length() < 13)
+	{
+		c = RFID.read();
+		msg += c;
+		Serial.print(msg.length());
+		Serial.print("   ");
+		Serial.println(msg);
+	}
+	if (msg.length() == 13)
+	{
+		Serial.println("---FINAL CARD---");
+		Serial.println(msg);
+		if (msg.equals(card))
+		{
+			digitalWrite(13, HIGH);
+			Serial.println("here");
+		}
+		RFID.end();
+		delay(2000);
+		RFID.begin(9600);
+		msg = "";
+	}
 }
